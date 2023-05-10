0a21c1403680435dbcbd11d92e6dd2e7fa2b143e
Matheus Silva Borges <129526862+MatthSB@users.noreply.github.com>
2023-05-09 23:53:02 -0300

    Recriando o js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function criptografar() {
+/*function criptografar() {
     let texto = document.getElementById("texts").value;
     if(texto == "") {
         document.getElementById("area2-text").style.visibility = "hidden";
@@ -59,4 +59,33 @@ function copy() {
     document.execCommand("copy");
     document.getElementById("texts").value = "";
     document.getElementById("area2-text").style.visibility = "hidden";
+}
+*/
+
+function criptografar() {
+    let text = document.getElementById("texts");
+    if (text == "") {
+        document.getElementById("with-text").style.display = "none";
+        alert("Digite algum texto.")
+    } else {
+        if (/[A-Z]/.test(text) && /[áàãâäéèêëíìîïóòõôöúùûü]/.test(text)) {
+            alert("Utilize somente letras MINÚSCULAS e SEM ACENTO.");
+        } else {
+            if (/[A-Z]/.test(text)) {
+                alert("Utilize somente letras MINÚSCULAS");
+            } else {
+                if (/[áàãâäéèêëíìîïóòõôöúùûü]/.test(text)) {
+                    alert("NÃO utilize acentos.");
+                } else {
+                    text = text.replaceAll("e","enter")
+                    text = text.replaceAll("i","imes");
+                    text = text.replaceAll("a","ai");
+                    text = text.replaceAll("o","ober");
+                    text = text.replaceAll("u","ufat");
+                    document.getElementById("with-text").style.display = "block";
+                    document.getElementById("text2").value = text;
+                }
+            }
+        }
+    }
 }
