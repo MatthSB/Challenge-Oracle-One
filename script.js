e534f2250e07cf0d79598aa5281be4c767b8a9db
Matheus Silva Borges <129526862+MatthSB@users.noreply.github.com>
2023-05-10 14:46:53 -0300

    Atualizando JavaScript

--- script.js
+++ script.js
@@ -1,91 +1,66 @@
-/*function criptografar() {
-    let texto = document.getElementById("texts").value;
-    if(texto == "") {
-        document.getElementById("area2-text").style.visibility = "hidden";
+function criptografar() {
+  let text = document.getElementById("texts").value;
+  if (text == "") {
+    alert("Digite algum texto.");
+  } else {
+    if (/[A-Z]/.test(text) && /[áàãâäéèêëíìîïóòõôöúùûü]/.test(text)) {
+      alert("Utilize somente letras MINÚSCULAS e SEM ACENTO.");
     } else {
-        if(/[A-Z]/.test(texto) && /[áàãâäéèêëíìîïóòõôöúùûü]/.test(texto)) {
-            alert("Não pode usar letras maiúsculas, nem usar acentos!")
+      if (/[A-Z]/.test(text)) {
+        alert("Utilize somente letras MINÚSCULAS.");
+      } else {
+        if (/[áàãâäéèêëíìîïóòõôöúùûü]/.test(text)) {
+          alert("NÃO utilize acentos.");
         } else {
-            if(/[A-Z]/.test(texto)) {
-                alert("Não pode usar letras maiúsculas!");
-            } else {
-                if(/[áàãâäéèêëíìîïóòõôöúùûü]/.test(texto)) {
-                    alert("Não pode usar acentos!");
-                } else {
-                    texto = texto.replaceAll("e","enter");
-                    texto = texto.replaceAll("i","imes");
-                    texto = texto.replaceAll("a","ai");
-                    texto = texto.replaceAll("o","ober");
-                    texto = texto.replaceAll("u","ufat");
-                    document.getElementById("area2-text").style.visibility = "visible";
-                    document.getElementById("text2").value = texto;
-                }
-            }
+          text = text.replaceAll("e", "enter");
+          text = text.replaceAll("i", "imes");
+          text = text.replaceAll("a", "ai");
+          text = text.replaceAll("o", "ober");
+          text = text.replaceAll("u", "ufat");
+          document.getElementById("no-text").style.display = "none";
+          document.getElementById("with-text").style.display = "block";
+          document.getElementById("text2").value = text;
         }
+      }
     }
+  }
 }
 
 function descriptografar() {
-    let texto = document.getElementById("texts").value;
-    if(texto == "") {
-        document.getElementById("area2-text").style.visibility = "hidden";
+  let text = document.getElementById("texts").value;
+  if (text == "") {
+    alert("Digite algum texto.");
+  } else {
+    if (/[A-Z]/.test(text) && /[áàãâäéèêëíìîïóòõôöúùûü]/.test(text)) {
+      alert("Utilize somente letras MINÚSCULAS e SEM ACENTO.");
     } else {
-        if(/[A-Z]/.test(texto) && /[áàãâäéèêëíìîïóòõôöúùûü]/.test(texto)) {
-            alert("Não pode usar letras maiúsculas, nem usar acentos!")
+      if (/[A-Z]/.test(text)) {
+        alert("Utilize somente letras MINÚSCULAS.");
+      } else {
+        if (/[áàãâäéèêëíìîïóòõôöúùûü]/.test(text)) {
+          alert("NÃO utilize acentos.");
         } else {
-            if(/[A-Z]/.test(texto)) {
-                alert("Não pode usar letras maiúsculas!");
-            } else {
-                if(/[áàãâäéèêëíìîïóòõôöúùûü]/.test(texto)) {
-                    alert("Não pode usar acentos!");
-                } else {
-                    texto = texto.replaceAll("enter","e");
-                    texto = texto.replaceAll("imes","i");
-                    texto = texto.replaceAll("ai","a");
-                    texto = texto.replaceAll("ober","o");
-                    texto = texto.replaceAll("ufat","u");
-                    document.getElementById("area2-text").style.visibility = "visible";
-                    document.getElementById("text2").value = texto;
-                }
-            }
+          text = text.replaceAll("enter", "e");
+          text = text.replaceAll("imes", "i");
+          text = text.replaceAll("ai", "a");
+          text = text.replaceAll("ober", "o");
+          text = text.replaceAll("ufat", "u");
+          document.getElementById("no-text").style.display = "none";
+          document.getElementById("with-text").style.display = "block";
+          document.getElementById("text2").value = text;
         }
+      }
     }
+  }
 }
 
 function copy() {
-    let textoCopiado = document.getElementById("text2");
-    textoCopiado.select();
-    textoCopiado.setSelectionRange(0, 99999)
-    document.execCommand("copy");
-    document.getElementById("texts").value = "";
-    document.getElementById("area2-text").style.visibility = "hidden";
-}
-*/
-
-function criptografar() {
-    let text = document.getElementById("texts");
-    if (text == "") {
-        document.getElementById("with-text").style.display = "none";
-        alert("Digite algum texto.")
-    } else {
-        if (/[A-Z]/.test(text) && /[áàãâäéèêëíìîïóòõôöúùûü]/.test(text)) {
-            alert("Utilize somente letras MINÚSCULAS e SEM ACENTO.");
-        } else {
-            if (/[A-Z]/.test(text)) {
-                alert("Utilize somente letras MINÚSCULAS");
-            } else {
-                if (/[áàãâäéèêëíìîïóòõôöúùûü]/.test(text)) {
-                    alert("NÃO utilize acentos.");
-                } else {
-                    text = text.replaceAll("e","enter")
-                    text = text.replaceAll("i","imes");
-                    text = text.replaceAll("a","ai");
-                    text = text.replaceAll("o","ober");
-                    text = text.replaceAll("u","ufat");
-                    document.getElementById("with-text").style.display = "block";
-                    document.getElementById("text2").value = text;
-                }
-            }
-        }
-    }
+  let copied = document.getElementById("text2");
+  copied.select();
+  copied.setSelectionRange(0, 99999);
+  document.execCommand("copy");
+  document.getElementById("texts").value = "";
+  document.getElementById("with-text").style.display = "none";
+  document.getElementById("no-text").style.display = "block";
+  alert("Texto copiado para a área de transferência.");
 }
